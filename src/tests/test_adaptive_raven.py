9ae772ee5a3fb6c1258440290d6e5dbe42ab836a
Debangshu Banerjee <debangshubanerjee1997@gmail.com>
2024-01-20 00:24:27 +0000

    More ICML results

--- src/tests/test_adaptive_raven.py
+++ src/tests/test_adaptive_raven.py
@@ -109,16 +109,16 @@ class TestRaven(TestCase):
 
     def test_mnist_uap_big(self):
         net_names = [config.MNIST_CONV_BIG]
-        eps = 0.165
-        for _ in range(10):
+        eps = 0.1
+        for _ in range(31):
             args = ver.RavenArgs(raven_mode=RavenMode.UAP, dataset=Dataset.MNIST, net_names=net_names,
-                count_per_prop=20, prop_count=10, eps=eps,
+                count_per_prop=10, prop_count=10, eps=eps,
                 threshold_execution=4, cross_executional_threshold=4, maximum_cross_execution_count=4, 
                 baseline_iteration=20, refinement_iterations=20, unroll_layers = False, unroll_layer_count=3, 
                 refine_intermediate_bounds =True, optimize_layers_count=2, 
                 bounds_for_individual_refinement=True, dataloading_seed = 0,
                 parallelize_executions=False,
-                device='cuda:2',
+                device='cuda:3',
                 lp_threshold=-1.5,
                 max_linear_apprx=3,
                 always_correct_cross_execution = False,
@@ -240,8 +240,8 @@ class TestRaven(TestCase):
 
     def test_cifar_uap_medium(self):
         net_names = [config.CIFAR_CROWN_IBP_MEDIUM]
-        eps = 2.0
-        for _ in range(15):
+        eps = 1.0
+        for _ in range(9):
             args = ver.RavenArgs(raven_mode=RavenMode.UAP, dataset=Dataset.CIFAR10, net_names=net_names,
                 count_per_prop=10, prop_count=10, eps=eps/255,
                 threshold_execution=4, cross_executional_threshold=4, maximum_cross_execution_count=4, 
@@ -255,14 +255,14 @@ class TestRaven(TestCase):
                 always_correct_cross_execution = False,
                 result_dir='icml_results', write_file=True)
             ver.adptiveRaven(raven_args=args)
-            eps += 0.5
+            eps += 0.25
 
     def test_cifar_uap_big(self):
         net_names = [config.CIFAR_CONV_BIG]
-        eps = 2.0
-        for _ in range(13):
+        eps = 0.5
+        for _ in range(9):
             args = ver.RavenArgs(raven_mode=RavenMode.UAP, dataset=Dataset.CIFAR10, net_names=net_names,
-                count_per_prop=10, prop_count=10, eps=eps/255,
+                count_per_prop=5, prop_count=20, eps=eps/255,
                 threshold_execution=3, cross_executional_threshold=3, maximum_cross_execution_count=3, 
                 baseline_iteration=20, refinement_iterations=20, unroll_layers = False, unroll_layer_count=3, 
                 refine_intermediate_bounds =True, optimize_layers_count=2, 
@@ -270,8 +270,8 @@ class TestRaven(TestCase):
                 parallelize_executions=False,
                 lp_threshold=-0.5,
                 max_linear_apprx=3,
-                device='cuda:2',
+                device='cuda:3',
                 always_correct_cross_execution = False,
                 result_dir='icml_results', write_file=True)
             ver.adptiveRaven(raven_args=args)
-            eps += 0.5
+            eps += 0.25
